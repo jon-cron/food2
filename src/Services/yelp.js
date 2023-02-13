ac302e286314cd37ab852b409cd4298572b7c641
jon-cron <jonathanacm95@gmail.com>
2023-02-13 10:20:39 -0700

    searching yelp is working

--- src/Services/yelp.js
+++ src/Services/yelp.js
@@ -5,4 +5,4 @@ export default axios.create({
   headers: {
     Authorization: 'Bearer XYBx7i9EfMzdNNY5a2FlrdKvybV5lu55EEVcOXqhtd1FnApGthmm_QEtURjYAwDlcVl5o8MJKnyu2VWkRUxHp_x8AZEixbseA9hH_doKwarOBaMp9tJ32FWdgkLqY3Yx'
   }
-})
+});
